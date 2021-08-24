790ca89b4122a9036d89ff1451ab38bb9ae9d9b1
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-24 11:17:30 +0530

    Added:Welcome Message

---
+++ test/java/com/cabinvoiceservice/InvoiceServiceTest.java
@@ -0,0 +1,7 @@
+package com.cabinvoiceservice;
+
+public class InvoiceServiceTest {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Cab Invoice Service Problem");
+    }
+}
