ee2327f7f1d9464cdf8adc174d1a6a8ae7aabe05
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-24 11:21:06 +0530

    Added:Program to Calculate Cab Service Invoice

--- test/java/com/cabinvoiceservice/InvoiceServiceTest.java
+++ test/java/com/cabinvoiceservice/InvoiceServiceTest.java
@@ -1,7 +1,15 @@
 package com.cabinvoiceservice;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class InvoiceServiceTest {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Cab Invoice Service Problem");
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25, fare, 0.0);
     }
 }
