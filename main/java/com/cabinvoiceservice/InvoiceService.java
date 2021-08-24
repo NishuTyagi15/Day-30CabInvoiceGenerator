6baa7fd6fe99c88932e212a1acbf60e50476723d
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-24 12:15:44 +0530

    Added:Program to Calculate Cab Service Invoice

---
+++ main/java/com/cabinvoiceservice/InvoiceService.java
@@ -0,0 +1,16 @@
+package com.cabinvoiceservice;
+
+public class InvoiceService {
+
+    private static final double MINFARE = 5;
+    private static final double MINCOSTPERKM = 10;
+    private static final int COSTPERTIME = 1;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINCOSTPERKM + time * COSTPERTIME;
+        if(totalFare < MINFARE) {
+            return MINFARE;
+        }
+        return totalFare;
+    }
+}
--- test/java/com/cabinvoiceservice/InvoiceServiceTest.java
+++ test/java/com/cabinvoiceservice/InvoiceServiceTest.java
@@ -6,10 +6,19 @@ import org.junit.jupiter.api.Test;
 public class InvoiceServiceTest {
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        double distance = 2.0;
-        int time = 5;
-        double fare = invoiceGenerator.calculateFare(distance, time);
-        Assertions.assertEquals(25, fare, 0.0);
+        InvoiceService invoiceService = new InvoiceService();
+        double distance = 8.0;
+        int time = 12;
+        double fare = invoiceService.calculateFare(distance, time);
+        Assertions.assertEquals(92, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double distance = 0.2;
+        int time = 2;
+        double totalFare = invoiceService.calculateFare(distance, time);
+        Assertions.assertEquals(5, totalFare, 0.0);
     }
 }
